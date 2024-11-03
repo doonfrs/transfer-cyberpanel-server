dd772c4c6403c6c4c234c9fe9e08b79035809664
doonfrs <doonfrs@gmail.com>
2024-11-03 17:49:18 +0300

    change folder name

--- migrate.sh
+++ migrate.sh
@@ -9,7 +9,7 @@ if [ ! -f "config.ini" ]; then
 fi
 
 
-php migrate-script/00-migrate-info.php
+php migrate-scripts/00-migrate-info.php
 
 echo "if You are sure about that, type 'yes' and press enter: ";
 read -r answer
@@ -20,11 +20,11 @@ fi
 
 echo "Running migrations..."    
 echo "Migrating websites..."
-php migrate-script/01-migrate-websites.php && \
+php migrate-scripts/01-migrate-websites.php && \
 echo "Migrating emails..." && \
-php migrate-script/02-migrate-emails.php && \
+php migrate-scripts/02-migrate-emails.php && \
 echo "Migrating websites data..." && \
-php migrate-script/03-migrate-websites-data.php && \
+php migrate-scripts/03-migrate-websites-data.php && \
 echo "Migrating vmail..." && \
-php migrate-script/04-migrate-websites-vmail.php && \
+php migrate-scripts/04-migrate-websites-vmail.php && \
 echo "All migrations completed." 
